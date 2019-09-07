4581b5e0afed7315d169b933cf152ca81428b8fd
jameskomo <james.komoh@gmail.com>
2019-09-07 14:31:01 +0300

    Migrations done from SQLite to PostgreSQL DB for larger transaction handling

--- myproject/settings.py
+++ myproject/settings.py
@@ -78,21 +78,21 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'bus_booking_system',
-#         'USER': 'komo',
-#         'PASSWORD': 'komo',
-#     }
-# }
-    
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bus_booking_system',
+        'USER': 'komo',
+        'PASSWORD': 'komo',
     }
 }
+    
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
